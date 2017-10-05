4b2b1756bdbb529ba6a90847923327aab60230d6
mikesanderson85 <32518598+mikesanderson85@users.noreply.github.com>
2017-10-05 13:22:56 +0100

    Update Azure_AD_Quick_Domain_With_VMs.ps1

--- Azure_AD_Quick_Domain_With_VMs.ps1
+++ Azure_AD_Quick_Domain_With_VMs.ps1
@@ -57,62 +57,62 @@ $rdpString = $rdpVM.DnsSettings.Fqdn + ':3389'
 Write-Host 'Connect to the VM using the URL below:' -foregroundcolor yellow -backgroundcolor red 
 Write-Host $rdpString
 
-if ($numberOfComputerstoCreate -gt 0){
-if (!$AzureAccount) {
-$AzureAccount = Login-AzureRmAccount
-}
+if ($numberOfVMsToCreate -gt 0){
+    if (!$AzureAccount) {
+    $AzureAccount = Login-AzureRmAccount
+    }
 
-$subs = Get-AzureRmSubscription 
-Select-AzureRmSubscription -TenantId $subs[0].TenantId -SubscriptionId $subs[0].SubscriptionId
+    $subs = Get-AzureRmSubscription 
+    Select-AzureRmSubscription -TenantId $subs[0].TenantId -SubscriptionId $subs[0].SubscriptionId
 
-# Create New Resource Group
-# Checks to see if RG exists
-# -ErrorAction Stop added to Get-AzureRmResourceGroup cmdlet to treat errors as terminating
+    # Create New Resource Group
+    # Checks to see if RG exists
+    # -ErrorAction Stop added to Get-AzureRmResourceGroup cmdlet to treat errors as terminating
  
-try {
-    Get-AzureRmResourceGroup -Name $rgName -Location $location -ErrorAction Stop
-} catch {
-    Write-Host "Resource Group doesn't exist" -foregroundcolor yellow -backgroundcolor red
-    throw 'An error occurred'
-}
-
-For ($i = 1;$i -le $numberOfVMsToCreate;$i++){
-
-$vmName = "$vmName$i"
-
- 
-# Check availability of DNS name
- 
-If ((Test-AzureRmDnsAvailability -DomainQualifiedName $vmName -Location $location) -eq $false) {
-        Write-Host 'The DNS label prefix for the VM is already in use' -foregroundcolor yellow -backgroundcolor red
+    try {
+        Get-AzureRmResourceGroup -Name $rgName -Location $location -ErrorAction Stop
+    } catch {
+        Write-Host "Resource Group doesn't exist" -foregroundcolor yellow -backgroundcolor red
         throw 'An error occurred'
-}
- 
-$newVMParams = @{
-    'ResourceGroupName' = $rgName
-    'TemplateURI' = 'https://raw.githubusercontent.com/mikesanderson85/Azure-Quick-Deploy/master/azuredeploy_domain_joined_VM.json'
-    'existingVNETName' = 'adVNET'
-    'existingSubnetName' = 'adSubnet'
-    'dnsLabelPrefix' = $vmName
-    'vmSize' = $vmsize
-    'domainToJoin' = $domainName
-    'domainUsername' = $adadmin
-    'domainPassword' = convertto-securestring $domainPassword -asplaintext -force
-    'ouPath' = ''
-    'domainJoinOptions' = 3
-    'vmAdminUsername' = $vmUser
-    'vmAdminPassword' = convertto-securestring $vmPassword -asplaintext -force
-}
-New-AzureRmResourceGroupDeployment @newVMParams
+    }
 
-# Display the RDP connection string
+    For ($i = 1;$i -le $numberOfVMsToCreate;$i++){
+
+    $vmName = "$vmName$i"
+
  
-$rdpVM = Get-AzureRmVM -ResourceGroupName $rgName -Name $vmName
+    # Check availability of DNS name
  
-$rdpString = $vmName + '.' + $rdpVM.Location + '.cloudapp.azure.com'
-Write-Host 'Connect to the VM using the URL below:' -foregroundcolor yellow -backgroundcolor red 
-Write-Host $rdpString
-}
+    If ((Test-AzureRmDnsAvailability -DomainQualifiedName $vmName -Location $location) -eq $false) {
+            Write-Host 'The DNS label prefix for the VM is already in use' -foregroundcolor yellow -backgroundcolor red
+            throw 'An error occurred'
+    }
+ 
+    $newVMParams = @{
+        'ResourceGroupName' = $rgName
+        'TemplateURI' = 'https://raw.githubusercontent.com/mikesanderson85/Azure-Quick-Deploy/master/azuredeploy_domain_joined_VM.json'
+        'existingVNETName' = 'adVNET'
+        'existingSubnetName' = 'adSubnet'
+        'dnsLabelPrefix' = $vmName
+        'vmSize' = $vmsize
+        'domainToJoin' = $domainName
+        'domainUsername' = $adadmin
+        'domainPassword' = convertto-securestring $domainPassword -asplaintext -force
+        'ouPath' = ''
+        'domainJoinOptions' = 3
+        'vmAdminUsername' = $vmUser
+        'vmAdminPassword' = convertto-securestring $vmPassword -asplaintext -force
+    }
+    New-AzureRmResourceGroupDeployment @newVMParams
+
+    # Display the RDP connection string
+ 
+    $rdpVM = Get-AzureRmVM -ResourceGroupName $rgName -Name $vmName
+ 
+    $rdpString = $vmName + '.' + $rdpVM.Location + '.cloudapp.azure.com'
+    Write-Host 'Connect to the VM using the URL below:' -foregroundcolor yellow -backgroundcolor red 
+    Write-Host $rdpString
+    }
 } else {
 Write-Host "No VM's will be created"
 }
